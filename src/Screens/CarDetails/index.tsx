839763703e1205ba92b87bacf9cf88df707c538b
Josias Silva Siqueira <josiassxz@hotmail.com>
2022-09-27 20:47:59 -0300

    finalizando interface detalhes

--- src/Screens/CarDetails/index.tsx
+++ src/Screens/CarDetails/index.tsx
@@ -6,6 +6,15 @@ import {
   Container,
   Header,
   CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Periodo,
+  Price,
+  About,
  } from './styles';
 
 export function CarDetails(){
@@ -19,6 +28,24 @@ export function CarDetails(){
             imagesUrl={['https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png']}
             />
          </CarImages> 
+
+         <Content>
+          <Details>
+            <Description>
+              <Brand>Lamborghini</Brand>
+              <Name>Huracan</Name>
+            </Description>
+            <Rent>
+              <Periodo>Ao dia</Periodo>
+              <Price>R$ 580</Price>
+            </Rent>
+          </Details>
+          <About>Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+             na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta 
+             de acelerar.</About>
+
+         </Content>
+
     </Container>
 
     );
